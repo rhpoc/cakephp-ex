f86488c42f8f445b0eead5e69be06dae47a60ce5
ouyangxiu <rrobinso@redhat.com>
2016-12-13 10:00:42 -0800

    Update hello.php

--- app/webroot/hello.php
+++ app/webroot/hello.php
@@ -1,1 +1,13 @@
-Hello
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Hello</title>
+    </head>
+    <body>
+
+        <?php
+            echo "Hello, I'm a PHP script!";
+        ?>
+
+    </body>
+</html>
